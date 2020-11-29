b1823d64e2baac17ca9aa63447da2a73a1ccad2f
Hardik17K <kumarhardik1708@gmail.com>
2020-11-29 18:32:59 +0530

    Added Game

---
+++ 2D Game Project Beginner/Assets/Scripts/LevelController.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private Enemy[] _enemies;
+    private static int _nextLevel = 1;
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();    
+    }
+
+    void Update()
+    {
+        foreach(Enemy enemy in _enemies)
+        {
+            if(enemy!=null)
+                return;
+        }  
+
+        Debug.Log("You Killed All Enemies");
+
+        _nextLevel++;
+        string nextLevelName = "Level" + _nextLevel;
+
+        SceneManager.LoadScene(nextLevelName);
+    }
+    
+}
